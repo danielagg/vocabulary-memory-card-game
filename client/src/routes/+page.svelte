324c444607755595da35a76194344bcb172f17d1
Daniel Agg <daniel.agg@outlook.com>
2022-10-29 11:41:33 +0200

    desktop design

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,4 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<h2 class="text-3xl font-bold underline">Hello Tailwind</h2>
+<main class="min-h-screen w-full bg-gray-100 text-gray-700 flex justify-center items-center">
 
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <div class="w-1/2 flex flex-col">
+
+        <div class="self-end flex flex-col items-end">
+            <h2 class="text-5xl font-bold text-sky-600">6</h2>
+            <p class="text-sm uppercase">Score</p>
+        </div>
+        
+        <div class="mt-12 bg-gray-50 rounded shadow p-12 flex flex-col items-center justify-center">
+            <h1 class="text-8xl font-bold my-12">Derecha</h1>
+
+            <div class="mt-10 w-full flex justify-between items-center">
+                <!-- <input class="w-full p-4 outline-0 bg-gray-100 border-b-2 border-l-2 border-gray-200 rounded-bl-xl text-sky-600 placeholder-gray-300" placeholder="Answer" type="text" /> -->
+                <input class="w-full p-4 outline-0 bg-gray-100 border-y-2 border-l-2 border-gray-200 rounded-bl-xl rounded-tl-xl placeholder-gray-300" placeholder="Answer" type="text" />
+                <button class="bg-gray-700 border-2 border-gray-700 hover:bg-sky-500 hover:border-sky-500 text-gray-100 cursor-pointer  py-4 px-12 font-bold">Submit</button>
+            </div>
+        </div>
+
+       
+    </div>
+
+</main>
+
