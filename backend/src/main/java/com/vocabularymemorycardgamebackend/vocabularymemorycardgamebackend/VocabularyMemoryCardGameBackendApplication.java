ac244227beea798006f3ce0a8fb5789928d094c9
Daniel Agg <daniel.agg@outlook.com>
2022-10-29 14:21:49 +0200

    spring security

--- backend/src/main/java/com/vocabularymemorycardgamebackend/vocabularymemorycardgamebackend/VocabularyMemoryCardGameBackendApplication.java
+++ backend/src/main/java/com/vocabularymemorycardgamebackend/vocabularymemorycardgamebackend/VocabularyMemoryCardGameBackendApplication.java
@@ -2,12 +2,51 @@ package com.vocabularymemorycardgamebackend.vocabularymemorycardgamebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
+//@CrossOrigin(origins = List.of("http://localhost:5173", "http://localhost:5173"))
 public class VocabularyMemoryCardGameBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VocabularyMemoryCardGameBackendApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	public class WebSecurityConfig {
+		@Bean
+		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+			http.cors();
+//			http.authorizeRequests()
+//					.anyRequest()
+//					.authenticated()
+//					.and()
+//					.httpBasic();
+			return http.build();
+		}
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("http://localhost:8080/", "http://localhost:5173/")); // Arrays.asList("https://example.com")
+		configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE","PATCH","OPTIONS"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,13 +1,18 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+  </script>
+
 <main class="min-h-screen w-full bg-gray-100 text-gray-700 flex justify-center lg:items-center">
     <div class="w-full lg:w-1/2 flex flex-col">
 
-        <div class="lg:self-end flex flex-col items-center lg:items-end mt-8 lg:mt-0">
+        <!-- <div class="lg:self-end flex flex-col items-center lg:items-end mt-8 lg:mt-0">
             <h2 class="text-5xl font-bold text-sky-600">6</h2>
             <p class="text-sm uppercase">Score</p>
-        </div>
+        </div> -->
         
         <div class="mt-8 lg:mt-12 mx-4 lg:mx-0 bg-gray-50 rounded shadow p-12 flex flex-col items-center justify-center">
-            <h1 class="text-5xl break-all lg:text-7xl font-bold my-12">Derecha</h1>
+            <h1 class="text-5xl break-all lg:text-7xl font-bold my-12">{@html data.word}</h1>
 
             <div class="mt-10 w-full flex flex-col lg:flex-row justify-between items-center space-y-2 lg:space-y-0">
                 <input class="w-full p-4 outline-0 bg-gray-100 border-2 lg:border-r-0 border-gray-200 rounded-lg lg:rounded-xl lg:rounded-br-none lg:rounded-tr-none placeholder-gray-300" placeholder="Answer" type="text" />
